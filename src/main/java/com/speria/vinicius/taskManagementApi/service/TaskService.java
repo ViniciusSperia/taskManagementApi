72644cbc57bb858e7f9a30a669b9c1c8edd0206a
ViniSperia <Vinisperia@gmail.com>
2024-08-02 22:25:27 +0100

    Add Cache and Swagger and fix problems in Pom

--- src/main/java/com/speria/vinicius/taskManagementApi/service/TaskService.java
+++ src/main/java/com/speria/vinicius/taskManagementApi/service/TaskService.java
@@ -3,11 +3,11 @@ package com.speria.vinicius.taskManagementApi.service;
 import com.speria.vinicius.taskManagementApi.model.Task;
 import com.speria.vinicius.taskManagementApi.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,6 +15,7 @@ public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
+    @Cacheable("tasks")
     public Page<Task> findAll(Pageable pageable) {
         return taskRepository.findAll(pageable);
         }
